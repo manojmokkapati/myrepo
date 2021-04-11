6e13cd3ac1170cfca73cfb0834fa2f8ac60b2329
manojmokkapati <81313832+manojmokkapati@users.noreply.github.com>
2021-04-11 22:08:44 +0530

    updats

--- scripts.js
+++ scripts.js
@@ -1,70 +1,94 @@
 // insert single Documents in mongodb
-db.player.insertOne({_id: "mokkapati",age: 65,batsmanType: "right",numberOfMatches: 688,shirtNumber: 990,teams: [
-    "india",
-    "ap",
-    "tamilNadu"
-],playerStats: [
-    {teamName: "india", totalScore: 274, numbeOfSixs: 7
-    },
-    {teamName: "ap", totalScore: 842, numbeOfFours: 89
-    },
-    {teamName: "tamil nadu", totalScore: 92, numbeOfWikets: 2
-    }
-]
+db.player.insertOne({
+    _id: "mokkapati", age: 65, batsmanType: "right", numberOfMatches: 688, shirtNumber: 990, teams: [
+        "india",
+        "ap",
+        "tamilNadu"
+    ], playerStats: [
+        {
+            teamName: "india", totalScore: 274, numbeOfSixs: 7
+        },
+        {
+            teamName: "ap", totalScore: 842, numbeOfFours: 89
+        },
+        {
+            teamName: "tamil nadu", totalScore: 92, numbeOfWikets: 2
+        }
+    ]
 })
 
 // insert many documents in mongodb scripts
 db.player.insertMany([
-    {_id: "mokkapati",age: 65,batsmanType: "right",numberOfMatches: 688,shirtNumber: 990,teams: [
+    {
+        _id: "mokkapati", age: 65, batsmanType: "right", numberOfMatches: 688, shirtNumber: 990, teams: [
             "india",
             "ap",
             "tamilNadu"
-        ],playerStats: [
-            {teamName: "india", totalScore: 274, numbeOfSixs: 7
+        ], playerStats: [
+            {
+                teamName: "india", totalScore: 274, numbeOfSixs: 7
             },
-            {teamName: "ap", totalScore: 842, numbeOfFours: 89
+            {
+                teamName: "ap", totalScore: 842, numbeOfFours: 89
             },
-            {teamName: "tamil nadu", totalScore: 92, numbeOfWikets: 2
+            {
+                teamName: "tamil nadu", totalScore: 92, numbeOfWikets: 2
             }
         ]
     },
-    {_id: "ramu",age: 48,batsmanType: "left",numberOfMatches: 98,shirtNumber: 98,teams: [
+    {
+        _id: "ramu", age: 48, batsmanType: "left", numberOfMatches: 98, shirtNumber: 98, teams: [
             "india",
             "ap",
             "tamilNadu"
-        ],playerStats: [
-            {teamName: "india", totalScore: 70004, numbeOfSixs: 97
+        ], playerStats: [
+            {
+                teamName: "india", totalScore: 70004, numbeOfSixs: 97
             },
-            {teamName: "ap", totalScore: 904622, numbeOfFours: 809
+            {
+                teamName: "ap", totalScore: 904622, numbeOfFours: 809
             },
-            {teamName: "tamil nadu", totalScore: 9022, numbeOfWikets: 89
+            {
+                teamName: "tamil nadu", totalScore: 9022, numbeOfWikets: 89
             }
         ]
     },
-    {_id: "jadeja",age: 42,batsmanType: "left",numberOfMatches: 98,shirtNumber: 8,teams: [
+    {
+        _id: "jadeja", age: 42, batsmanType: "left", numberOfMatches: 98, shirtNumber: 8, teams: [
             "india",
             "ap",
             "tamilNadu"
-        ],playerStats: [
-            {teamName: "india", totalScore: 888888, numbeOfSixs: 888
+        ], playerStats: [
+            {
+                teamName: "india", totalScore: 888888, numbeOfSixs: 888
             },
-            {teamName: "ap", totalScore: 8972, numbeOfFours: 980
+            {
+                teamName: "ap", totalScore: 8972, numbeOfFours: 980
             },
-            {teamName: "tamil nadu", totalScore: 10892, numbeOfWikets: 8
+            {
+                teamName: "tamil nadu", totalScore: 10892, numbeOfWikets: 8
             }
         ]
     },
-    {_id: "rohith",age: 42,batsmanType: "right",numberOfMatches: 28,shirtNumber: 90,teams: [
+    {
+        _id: "rohith", age: 42, batsmanType: "right", numberOfMatches: 28, shirtNumber: 90, teams: [
             "india",
             "ap",
             "tamilNadu"
-        ],playerStats: [
-            {teamName: "india", totalScore: 709274, numbeOfSixs: 77
+        ], playerStats: [
+            {
+                teamName: "india", totalScore: 709274, numbeOfSixs: 77
             },
-            {teamName: "ap", totalScore: 84622, numbeOfFours: 890
+            {
+                teamName: "ap", totalScore: 84622, numbeOfFours: 890
             },
-            {teamName: "tamil nadu", totalScore: 98022, numbeOfWikets: 82
+            {
+                teamName: "tamil nadu", totalScore: 98022, numbeOfWikets: 82
             }
         ]
     }
 ])
+
+
+// db.player.find() 
+//db.player.find().pretty()

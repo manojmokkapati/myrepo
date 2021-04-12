dfa4bedf239d01ec9c2e728c1ddb2772ea2754e4
manojmokkapati <81313832+manojmokkapati@users.noreply.github.com>
2021-04-12 14:02:52 +0530

    updates

--- scriptsEditor.js
+++ scriptsEditor.js
@@ -1,53 +1,291 @@
 db.player.insertMany([
-    {_id: "mokkapati",age: 65,batsmanType: "right",numberOfMatches: 688,shirtNumber: 990,teams: [
+
+    
+    {
+        _id: "Harbhajan Singh", age: 38, batsmanType: "right", numberOfMatches: 28, shirtNumber: 90, teams: [
             "india",
             "ap",
             "tamilNadu"
-        ],playerStats: [
-            {teamName: "india", totalScore: 274, numbeOfSixs: 7
+        ], playerStats: [
+            {
+                teamName: "india", totalScore: 70974, numbeOfSixs: 77
             },
-            {teamName: "ap", totalScore: 842, numbeOfFours: 89
+            {
+                teamName: "ap", totalScore: 8462, numbeOfFours: 890
             },
-            {teamName: "tamil nadu", totalScore: 92, numbeOfWikets: 2
+            {
+                teamName: "tamil nadu", totalScore: 9822, numbeOfWikets: 82
+            }
+        ]
+    }, {
+        _id: "Prasidh Krishna", age: 25, batsmanType: "right", numberOfMatches: 1215, shirtNumber: 126, teams: [
+            "india",
+            "ap",
+            "tamilNadu"
+        ], playerStats: [
+            {
+                teamName: "india", totalScore: 1264, numbeOfSixs: 17
+            },
+            {
+                teamName: "ap", totalScore: 942, numbeOfFours: 69
+            },
+            {
+                teamName: "tamil nadu", totalScore: 920, numbeOfWikets: 20
             }
         ]
     },
-    {_id: "ramu",age: 48,batsmanType: "left",numberOfMatches: 98,shirtNumber: 98,teams: [
-            "india",
+    {
+        _id: "Pat Cummins", age: 27, batsmanType: "right", numberOfMatches: 908, shirtNumber: 8, teams: [
+            "Australia",
             "ap",
             "tamilNadu"
-        ],playerStats: [
-            {teamName: "india", totalScore: 70004, numbeOfSixs: 97
+        ], playerStats: [
+            {
+                teamName: "Australia", totalScore: 2804, numbeOfSixs: 197
             },
-            {teamName: "ap", totalScore: 904622, numbeOfFours: 809
+            {
+                teamName: "ap", totalScore: 722, numbeOfFours: 89
             },
-            {teamName: "tamil nadu", totalScore: 9022, numbeOfWikets: 89
+            {
+                teamName: "tamil nadu", totalScore: 93, numbeOfWikets: 9
             }
         ]
     },
-    {_id: "jadeja",age: 42,batsmanType: "left",numberOfMatches: 98,shirtNumber: 8,teams: [
+    {
+        _id: "Varun Chakravarthy", age: 29, batsmanType: "right", numberOfMatches: 98, shirtNumber: 8, teams: [
             "india",
             "ap",
             "tamilNadu"
-        ],playerStats: [
-            {teamName: "india", totalScore: 888888, numbeOfSixs: 888
+        ], playerStats: [
+            {
+                teamName: "india", totalScore: 888078, numbeOfSixs: 8829
             },
-            {teamName: "ap", totalScore: 8972, numbeOfFours: 980
+            {
+                teamName: "ap", totalScore: 897, numbeOfFours: 98
             },
-            {teamName: "tamil nadu", totalScore: 10892, numbeOfWikets: 8
+            {
+                teamName: "tamil nadu", totalScore: 1089, numbeOfWikets: 8
             }
         ]
     },
-    {_id: "rohith",age: 42,batsmanType: "right",numberOfMatches: 28,shirtNumber: 90,teams: [
+    {
+        _id:"Sanju Samson", age: 26, batsmanType: "right", numberOfMatches: 1208, shirtNumber: 990, teams: [
             "india",
             "ap",
             "tamilNadu"
-        ],playerStats: [
-            {teamName: "india", totalScore: 709274, numbeOfSixs: 77
+        ], playerStats: [
+            {
+                teamName: "india", totalScore: 70974, numbeOfSixs: 733
             },
-            {teamName: "ap", totalScore: 84622, numbeOfFours: 890
+            {
+                teamName: "ap", totalScore: 8422, numbeOfFours: 89
             },
-            {teamName: "tamil nadu", totalScore: 98022, numbeOfWikets: 82
+            {
+                teamName: "tamil nadu", totalScore: 9802, numbeOfWikets: 82
+            }
+        ]
+    },
+    {
+        _id: "Ben Stokes", age: 29, batsmanType: "right", numberOfMatches: 88, shirtNumber: 13, teams: [
+            "England",
+            "ap",
+            "tamilNadu"
+        ], playerStats: [
+            {
+                teamName: "England", totalScore: 274, numbeOfSixs: 7
+            },
+            {
+                teamName: "ap", totalScore: 842, numbeOfFours: 8
+            },
+            {
+                teamName: "tamil nadu", totalScore: 92, numbeOfWikets: 2
+            }
+        ]
+    },
+    {
+        _id: "Riyan Parag", age: 19, batsmanType: "right", numberOfMatches: 498, shirtNumber: 918, teams: [
+            "india",
+            "ap",
+            "tamilNadu"
+        ], playerStats: [
+            {
+                teamName: "india", totalScore: 71234, numbeOfSixs: 970
+            },
+            {
+                teamName: "ap", totalScore: 9046, numbeOfFours: 809
+            },
+            {
+                teamName: "tamil nadu", totalScore: 902, numbeOfWikets: 89
+            }
+        ]
+    },
+    {
+        _id: "Shivam Dube", age: 27, batsmanType: "right", numberOfMatches: 98, shirtNumber: 8, teams: [
+            "india",
+            "ap",
+            "tamilNadu"
+        ], playerStats: [
+            {
+                teamName: "india", totalScore: 8288, numbeOfSixs: 118
+            },
+            {
+                teamName: "ap", totalScore: 892, numbeOfFours: 918
+            },
+            {
+                teamName: "tamil nadu", totalScore: 12, numbeOfWikets: 82
+            }
+        ]
+    },
+    {
+        _id: "Rahul Tewatia", age: 27, batsmanType: "left", numberOfMatches: 28, shirtNumber: 1, teams: [
+            "india",
+            "ap",
+            "tamilNadu"
+        ], playerStats: [
+            {
+                teamName: "india", totalScore: 71092, numbeOfSixs: 177
+            },
+            {
+                teamName: "ap", totalScore: 842, numbeOfFours: 85
+            },
+            {
+                teamName: "tamil nadu", totalScore: 980, numbeOfWikets: 82
+            }
+        ]
+    },
+    {
+        _id: "Chris Morris", age: 31, batsmanType: "right", numberOfMatches: 688, shirtNumber: 990, teams: [
+            "South Africa",
+            "ap",
+            "tamilNadu"
+        ], playerStats: [
+            {
+                teamName: "South Africa", totalScore: 274, numbeOfSixs: 7
+            },
+            {
+                teamName: "ap", totalScore: 8429, numbeOfFours: 89
+            },
+            {
+                teamName: "tamil nadu", totalScore: 920, numbeOfWikets: 29
+            }
+        ]
+    },
+    {
+        _id: "Liam Livingstone ", age:30 , batsmanType: "right", numberOfMatches: 98, shirtNumber: 98, teams: [
+    
+            "England",
+            "ap",
+            "tamilNadu"
+        ], playerStats: [
+            {
+                teamName: "England", totalScore: 70004, numbeOfSixs: 917
+            },
+            {
+                teamName: "ap", totalScore: 92522, numbeOfFours: 810
+            },
+            {
+                teamName: "tamil nadu", totalScore: 9022, numbeOfWikets: 89
+            }
+        ]
+    },
+    {
+        _id: "Shreyas Gopal", age: 27, batsmanType: "right", numberOfMatches: 288, shirtNumber: 898, teams: [
+            "india",
+            "ap",
+            "tamilNadu"
+        ], playerStats: [
+            {
+                teamName: "india", totalScore: 1128, numbeOfSixs: 88
+            },
+            {
+                teamName: "ap", totalScore: 1220, numbeOfFours: 980
+            },
+            {
+                teamName: "tamil nadu", totalScore: 1892, numbeOfWikets: 890
+            }
+        ]
+    },
+    {
+        _id: "Jaydev Unadkat", age: 29, batsmanType: "right", numberOfMatches: 28, shirtNumber: 90, teams: [
+            "india",
+            "ap",
+            "tamilNadu"
+        ], playerStats: [
+            {
+                teamName: "india", totalScore: 70974, numbeOfSixs: 77
+            },
+            {
+                teamName: "ap", totalScore: 8462, numbeOfFours: 890
+            },
+            {
+                teamName: "tamil nadu", totalScore: 9822, numbeOfWikets: 82
+            }
+        ]
+    }, {
+        _id: "Kartik Tyagi", age: 20, batsmanType: "right", numberOfMatches: 125, shirtNumber: 126, teams: [
+            "india",
+            "ap",
+            "tamilNadu"
+        ], playerStats: [
+            {
+                teamName: "india", totalScore: 264, numbeOfSixs: 71
+            },
+            {
+                teamName: "ap", totalScore: 942, numbeOfFours: 69
+            },
+            {
+                teamName: "tamil nadu", totalScore: 920, numbeOfWikets: 20
+            }
+        ]
+    },
+    {
+        _id: "Mustafizur Rahman", age: 34, batsmanType: "left0", numberOfMatches: 908, shirtNumber: 8, teams: [
+            "Bangladesh",
+            "ap",
+            "tamilNadu"
+        ], playerStats: [
+            {
+                teamName: "Bangladesh", totalScore: 70004, numbeOfSixs: 97
+            },
+            {
+                teamName: "ap", totalScore: 7722, numbeOfFours: 809
+            },
+            {
+                teamName: "tamil nadu", totalScore: 962, numbeOfWikets: 89
+            }
+        ]
+    },
+    {
+        _id: "Andrew Tye", age: 29, batsmanType: "left", numberOfMatches: 98, shirtNumber: 8, teams: [
+            "Australia",
+            "ap",
+            "tamilNadu"
+        ], playerStats: [
+            {
+                teamName: "Australia", totalScore: 88878, numbeOfSixs: 882
+            },
+            {
+                teamName: "ap", totalScore: 897, numbeOfFours: 98
+            },
+            {
+                teamName: "tamil nadu", totalScore: 1089, numbeOfWikets: 8
+            }
+        ]
+    },
+    {
+        _id:"Jofra Archer", age: 31, batsmanType: "right", numberOfMatches: 98, shirtNumber: 90, teams: [
+            "England",
+            "ap",
+            "tamilNadu"
+        ], playerStats: [
+            {
+                teamName: "England", totalScore: 70974, numbeOfSixs: 72
+            },
+            {
+                teamName: "ap", totalScore: 8422, numbeOfFours: 821
+            },
+            {
+                teamName: "tamil nadu", totalScore: 9802, numbeOfWikets: 82
             }
         ]
     }
